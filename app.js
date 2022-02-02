ce0dd5dee15d58b57460ad66230896afb2ea5fdd
Katana <katanaosu@vk.com>
2022-02-02 17:58:39 +0300

    Resolved issue with ball direction after collision; Everything is in OOP-style now; Added TODO

--- app.js
+++ app.js
@@ -1,99 +1,7 @@
-const grid = document.querySelector('.grid');
-const scoreDisplay = document.querySelector('.score');
-const scoreBlock = document.querySelector('.scoreBlock');
-const colors = ['#C1E1DC', '#FFCCAC', '#FFEB94', '#FDD475'];
-
-let xDirection = 2;
-let yDirection = 2;
-let score = 0;
-
-class Object {
-    constructor(offsetLeft, offsetBottom) {
-        this.offsetLeft = offsetLeft;
-        this.offsetBottom = offsetBottom;
-    }
-}
-
-class Ball extends Object {
-    constructor(offsetLeft, offsetBottom) {
-        super(offsetLeft, offsetBottom);
-        this.ball = document.createElement('div');
-        this.ball.classList.add('ball');
-        this.ball.style.left = this.offsetLeft + 'px';
-        this.ball.style.bottom = this.offsetBottom + 'px';
-        grid.append(this.ball);
-    }
-
-
-    static diameter = 20;
-
-    draw() {
-        this.ball.style.left = this.offsetLeft + 'px';
-        this.ball.style.bottom = this.offsetBottom + 'px';
-    }
-
-    move() {
-        this.offsetLeft += xDirection;
-        this.offsetBottom += yDirection;
-        this.draw();
-    }
-}
-
-class User extends Object {
-    constructor(offsetLeft, offsetBottom) {
-        super(offsetLeft, offsetBottom);
-        this.platform = document.createElement('div');
-        this.platform.classList.add('user');
-        this.draw();
-        grid.append(this.platform);
-    }
-
-    move = (e) => {
-        switch (e.key) {
-            case 'ArrowLeft':
-                if (this.offsetLeft > 0) {
-                    this.offsetLeft -= 10;
-                    this.draw();
-                }
-                break;
-            case 'ArrowRight': {
-                if (this.offsetLeft < GameField.boardWidth - Block.blockWidth) {
-                    this.offsetLeft += 10;
-                    this.draw();
-                }
-                break;
-            }
-        }
-    }
-
-    draw = () => {
-        this.platform.style.left = this.offsetLeft + 'px';
-        this.platform.style.bottom = this.offsetBottom + 'px';
-    }
-}
-
-class Block {
-    constructor(xAxis, yAxis) {
-        this.bottomLeft = [xAxis, yAxis];
-        this.bottomRight = [Block.blockWidth + xAxis, yAxis];
-        this.topLeft = [xAxis, Block.blockHeight + yAxis];
-        this.topRight = [xAxis + Block.blockWidth, yAxis + Block.blockHeight];
-        this.block = document.createElement('div');
-        this.block.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-        this.block.classList.add('block');
-        this.block.style.left = this.bottomLeft[0] + 'px';
-        this.block.style.bottom = this.bottomLeft[1] + 'px';
-        grid.append(this.block);
-    }
-
-    static blockWidth = 100;
-    static blockHeight = 20;
-}
-
 class GameField {
     constructor() {
         this.user = new User(230, 10);
-        this.ball = new Ball(230, 60);
+        this.ball = new Ball(230, 30);
         this.blocks = [
             new Block(10, 270),
             new Block(120, 270),
@@ -115,15 +23,20 @@ class GameField {
         this.timerId = setInterval(this.renderScene, 20);
     }
 
-    static boardWidth = parseInt(getComputedStyle(grid).width);
-    static boardHeight = parseInt(getComputedStyle(grid).height);
+    static grid = document.querySelector('.grid');
+    static scoreDisplay = document.querySelector('.score');
+    static scoreBlock = document.querySelector('.scoreBlock');
+    static score = 0;
+    static boardWidth = parseInt(getComputedStyle(GameField.grid).width);
+    static boardHeight = parseInt(getComputedStyle(GameField.grid).height);
+    static colors = ['#C1E1DC', '#FFCCAC', '#FFEB94', '#FDD475'];
 
     renderScene = () => {
         this.ball.move();
         this.checkForCollision();
     }
 
-    checkForCollision() {
+    checkForCollision = () => {
         // Check for block collisions
         for (let index = 0; index < this.blocks.length; index++) {
             if (
@@ -135,14 +48,11 @@ class GameField {
                 const allBlocks = Array.from(document.querySelectorAll('.block'));
                 allBlocks[index].classList.remove('block');
                 this.blocks.splice(index, 1);
-                changeDirectionBlocks();
-                score++;
-                scoreDisplay.textContent = String(score);
+                this.changeDirectionBlocks();
+                GameField.scoreDisplay.textContent = String(++GameField.score);
 
                 if (this.blocks.length === 0) {
-                    scoreBlock.textContent = `You win! Your score is ${score}`;
-                    clearInterval(this.timerId);
-                    document.removeEventListener('keydown', this.user.move);
+                    this.finishGame(`You win! Your score is ${GameField.score}`);
                 }
             }
         }
@@ -152,7 +62,7 @@ class GameField {
             (this.ball.offsetLeft > this.user.offsetLeft && this.ball.offsetLeft < this.user.offsetLeft + Block.blockWidth) &&
             (this.ball.offsetBottom > this.user.offsetBottom && this.ball.offsetBottom < this.user.offsetBottom + Block.blockHeight)
         ) {
-            changeDirectionUser();
+            this.changeDirectionUser();
         }
 
         // Check for wall collisions
@@ -160,58 +70,149 @@ class GameField {
             this.ball.offsetLeft >= (GameField.boardWidth - Ball.diameter) ||
             this.ball.offsetBottom >= (GameField.boardHeight - Ball.diameter) ||
             this.ball.offsetLeft <= 0) {
-            changeDirectionWalls();
+            this.changeDirectionWalls();
         }
 
         // Check for game over
         if (this.ball.offsetBottom <= 0) {
-            clearInterval(this.timerId);
-            scoreBlock.textContent = 'Game is over!';
-            document.removeEventListener('keydown', this.user.move);
+            this.finishGame('Game is over!');
+        }
+    }
+
+    changeDirectionWalls = () => {
+        if (this.ball.moveDirection.x > 0 && this.ball.moveDirection.y > 0) {
+            this.ball.moveDirection.x = -this.ball.moveDirection.x;
+            return;
+        }
+        if (this.ball.moveDirection.x > 0 && this.ball.moveDirection.y < 0) {
+            this.ball.moveDirection.x = -this.ball.moveDirection.x;
+            return;
+        }
+        if (this.ball.moveDirection.x < 0 && this.ball.moveDirection.y < 0) {
+            this.ball.moveDirection.x = -this.ball.moveDirection.x;
+            return;
+        }
+        if (this.ball.moveDirection.x < 0 && this.ball.moveDirection.y > 0) {
+            this.ball.moveDirection.x = -this.ball.moveDirection.x;
+            return;
+        }
+    }
+
+    changeDirectionBlocks = () => {
+        if (this.ball.moveDirection.x > 0 && this.ball.moveDirection.y > 0) {
+            this.ball.moveDirection.y = -this.ball.moveDirection.y;
+            return;
+        }
+        if (this.ball.moveDirection.x < 0 && this.ball.moveDirection.y > 0) {
+            this.ball.moveDirection.y = -this.ball.moveDirection.y;
+            return;
+        }
+    }
+
+    changeDirectionUser = () => {
+        if (this.ball.moveDirection.x > 0 && this.ball.moveDirection.y < 0) {
+            this.ball.moveDirection.y = -this.ball.moveDirection.y;
+            return;
+        }
+        if (this.ball.moveDirection.x < 0 && this.ball.moveDirection.y < 0) {
+            this.ball.moveDirection.y = -this.ball.moveDirection.y;
+            return;
+        }
+    }
+
+    finishGame = (text) => {
+        clearInterval(this.timerId);
+        GameField.scoreBlock.textContent = text;
+        document.removeEventListener('keydown', this.user.move);
+    }
+}
+
+class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Unit {
+    constructor(offsetLeft, offsetBottom, styledClass) {
+        this.offsetLeft = offsetLeft;
+        this.offsetBottom = offsetBottom;
+        // this.bottomLeft = new Point(offsetLeft, offsetBottom);
+        this.element = document.createElement('div');
+        this.element.classList.add(styledClass);
+        this.redraw();
+        GameField.grid.append(this.element);
+    }
+
+    redraw = () => {
+        this.element.style.left = this.offsetLeft + 'px';
+        this.element.style.bottom = this.offsetBottom + 'px';
+    }
+}
+
+class Ball extends Unit {
+    constructor(offsetLeft, offsetBottom) {
+        super(offsetLeft, offsetBottom, 'ball');
+        this.moveDirection = new Vector(3, 3);
+    }
+
+    static diameter = 20;
+
+    move = () => {
+        this.offsetLeft += this.moveDirection.x;
+        this.offsetBottom += this.moveDirection.y;
+        this.redraw();
+    }
+}
+
+class User extends Unit {
+    constructor(offsetLeft, offsetBottom) {
+        super(offsetLeft, offsetBottom, 'user');
+    }
+
+    move = (e) => {
+        switch (e.key) {
+            case 'ArrowLeft':
+                if (this.offsetLeft > 0) {
+                    this.offsetLeft -= 10;
+                    this.redraw();
+                }
+                break;
+            case 'ArrowRight': {
+                if (this.offsetLeft < GameField.boardWidth - Block.blockWidth) {
+                    this.offsetLeft += 10;
+                    this.redraw();
+                }
+                break;
+            }
         }
     }
 }
 
-const changeDirectionWalls = () => {
-    if (xDirection === 2 && yDirection === 2) {
-        xDirection = -2;
-        return;
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis];
+        this.bottomRight = [Block.blockWidth + xAxis, yAxis];
+        this.topLeft = [xAxis, Block.blockHeight + yAxis];
+        this.topRight = [xAxis + Block.blockWidth, yAxis + Block.blockHeight];
+        this.block = document.createElement('div');
+        this.block.style.backgroundColor = GameField.colors[Math.floor(Math.random() * GameField.colors.length)];
+        this.block.classList.add('block');
+        this.block.style.left = this.bottomLeft[0] + 'px';
+        this.block.style.bottom = this.bottomLeft[1] + 'px';
+        GameField.grid.append(this.block);
     }
-    if (xDirection === 2 && yDirection === -2) {
-        xDirection = -2;
-        return;
-    }
-    if (xDirection === -2 && yDirection === -2) {
-        xDirection = 2;
-        return;
-    }
-    if (xDirection === -2 && yDirection === 2) {
-        xDirection = 2;
-        return;
-    }
-}
 
-const changeDirectionBlocks = () => {
-    if (xDirection === 2 && yDirection === 2) {
-        yDirection = -2;
-        return;
-    }
-    if (xDirection === -2 && yDirection === 2) {
-        yDirection = -2;
-        return;
-    }
-}
-
-const changeDirectionUser = () => {
-    if (xDirection === 2 && yDirection === -2) {
-        yDirection = 2;
-        return;
-    }
-    if (xDirection === -2 && yDirection === -2) {
-        yDirection = 2;
-        return;
-    }
+    static blockWidth = 100;
+    static blockHeight = 20;
 }
 
 const gameField = new GameField();
 gameField.renderScene();
+/*TODO: разобраться с коллизией с блоками (дописать условия)
+ * разобраться с верхней стенкой (поменять проверку с offsetBottom)
+ * енум для кнопок со сложностью
+ * динамическая генерация блоков
+*/
+// class Block extends Unit
